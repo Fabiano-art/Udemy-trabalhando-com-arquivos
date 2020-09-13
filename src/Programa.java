ce494447bdbdf98c28518a4fb76d0a2c719f710f
Fabiano Guimarães <fabio.dub@outlook.com>
2020-09-12 21:05:57 -0300

    Adicionando o gitignore

---
+++ src/Programa.java
@@ -0,0 +1,28 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+		File file = new File("C:\\Users\\Guimarães\\Desktop\\Faculdade\\temp\\entrada.txt");
+		Scanner sc = null;
+		
+		try {
+			sc = new Scanner(file);
+			while(sc.hasNextLine()) {
+				System.out.println(sc.nextLine());
+			}
+		}
+		catch (IOException e) {
+			System.out.println("Erro: " + e.getMessage());
+		}
+		finally {
+			if(sc != null) {
+				sc.close();
+			}
+		}
+	}
+
+}
