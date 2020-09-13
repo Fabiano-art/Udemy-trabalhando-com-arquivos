e6d0ea631489ee136b23a3da43f441983dbd44d4
Fabiano Guimarães <fabio.dub@outlook.com>
2020-09-12 22:22:01 -0300

    Manipulando pastas

---
+++ src/Programa4.java
@@ -0,0 +1,27 @@
+import java.io.File;
+
+public class Programa4 {
+
+	public static void main(String[] args) {
+		
+		File path = new File("C:\\Users\\Guimarães\\Desktop\\Faculdade");
+		
+		File[] folders = path.listFiles(File::isDirectory);
+		
+		System.out.println("FOLDERS: ");
+		for(File folder : folders) {
+			System.out.println(folder);
+		}
+		
+		File[] files = path.listFiles(File::isFile);
+		
+		System.out.println("FILES: ");
+		for(File file : files) {
+			System.out.println(file);
+		}
+		
+		boolean success = new File("C:\\Users\\Guimarães\\Desktop\\Faculdade\\temp\\sub").mkdir();
+		
+	}
+
+}
